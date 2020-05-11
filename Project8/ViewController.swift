9a3e3fd4d8c08b9bad70d5d87b40a5d839c6a00b
Dennis <nndenninn@gmail.com>
2020-05-11 11:53:08 +0300

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Dennis Nesanoff on 11.05.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
